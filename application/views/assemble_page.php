f60a526e9ab8fc57e3030ad25d922cb8d66b24da
markbatin <markbatin@gmail.com>
2016-02-14 17:02:24 -0800

    assembly Signed-off-by:markbatin <markbatin@gmail.com> Signed-off-by:markbatin <markbatin@gmail.com>

--- application/views/assemble_page.php
+++ application/views/assemble_page.php
@@ -22,6 +22,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <tr><td>Torso<select class="buttonAssemble"><option>11a-1</option></select></td></tr>
             <tr><td>Legs<select class="buttonAssemble"><option>11a-2</option></select></td></tr>
             <tr><td><button type="button" class="buttonAssemble">ASSEMBLE</button></td></tr>
+            <tr></tr>
+            <br/>
+            <tr><td><h5 id="previewH5">Preview</h5></td></tr>
         </table>
     </div>
 </div>
